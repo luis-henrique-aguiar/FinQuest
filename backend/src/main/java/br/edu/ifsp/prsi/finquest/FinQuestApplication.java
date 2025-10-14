694c4a5c61c1ab68dcb1a4ae487fcbbdd777610d
Luis Henrique Aguiar <luishenriqueaguiar10@gmail.com>
2025-10-14 11:16:58 -0300

    Criação e configuração inicial do projeto

---
+++ backend/src/main/java/br/edu/ifsp/prsi/finquest/FinQuestApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prsi.finquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinQuestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/ifsp/prsi/finquest/FinQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prsi.finquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
