32088cc67ac1358cf33ab3c29cef7118b2eb55d7
matheus mantovani <mantovanimatheus335@gmail.com>
2025-10-27 18:04:59 -0300

    feat(auth): implement registerUser in UserController

--- backend/src/main/java/br/edu/ifsp/prsi/finquest/controller/UserController.java
+++ backend/src/main/java/br/edu/ifsp/prsi/finquest/controller/UserController.java
@@ -1,7 +1,14 @@
 package br.edu.ifsp.prsi.finquest.controller;
 
+import br.edu.ifsp.prsi.finquest.dto.RegisterUserDTO;
+import br.edu.ifsp.prsi.finquest.model.User;
 import br.edu.ifsp.prsi.finquest.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<Void> registerUser(@RequestBody @Valid RegisterUserDTO registerUserDTO) {
+        userService.registerUser(registerUserDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
