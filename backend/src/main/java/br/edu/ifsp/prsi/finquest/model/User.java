2f1480b914ba065c69cc28752cde0d428b4f0c4d
matheus mantovani <mantovanimatheus335@gmail.com>
2025-10-27 17:18:15 -0300

    feat(auth): create User entity

--- backend/src/main/java/br/edu/ifsp/prsi/finquest/model/User.java
+++ backend/src/main/java/br/edu/ifsp/prsi/finquest/model/User.java
@@ -1,11 +1,37 @@
 package br.edu.ifsp.prsi.finquest.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
 
 @Entity
+@Table(name = "User")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "total_fin_points")
+    private int totalFinPoints;
+
+    @Column(name = "budget", precision = 10, scale = 2)
+    private BigDecimal budget;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
 }
