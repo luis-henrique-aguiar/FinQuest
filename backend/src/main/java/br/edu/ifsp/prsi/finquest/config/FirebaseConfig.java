89428baf873942e096692cbec9f9863a44cf69f5
Luis Henrique Aguiar <luishenriqueaguiar10@gmail.com>
2025-11-03 10:00:35 -0300

    cofing: Removendo os beans de configuração do perfil de testes

--- backend/src/main/java/br/edu/ifsp/prsi/finquest/config/FirebaseConfig.java
+++ backend/src/main/java/br/edu/ifsp/prsi/finquest/config/FirebaseConfig.java
@@ -5,11 +5,13 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
     @Bean
--- backend/src/main/java/br/edu/ifsp/prsi/finquest/security/FirebaseTokenFilter.java
+++ backend/src/main/java/br/edu/ifsp/prsi/finquest/security/FirebaseTokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 @Component
+@Profile("!test")
 public class FirebaseTokenFilter extends OncePerRequestFilter {
 
     @Override
