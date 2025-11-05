e5ed3dfd3c93d1b2f843bb5bee12e1218aef5ecd
Luis Henrique Aguiar <luishenriqueaguiar10@gmail.com>
2025-11-05 09:19:38 -0300

    feat: Criação da entidade de lição

---
+++ backend/src/main/java/br/edu/ifsp/prsi/finquest/model/Lesson.java
@@ -0,0 +1,86 @@
+package br.edu.ifsp.prsi.finquest.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "lessons")
+public class Lesson {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer recFinPoints;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    public Lesson() {}
+
+    public Lesson(String id, String title, Integer recFinPoints, Course course) {
+        this.id = id;
+        this.title = title;
+        this.recFinPoints = recFinPoints;
+        this.course = course;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getRecFinPoints() {
+        return recFinPoints;
+    }
+
+    public void setRecFinPoints(Integer recFinPoints) {
+        this.recFinPoints = recFinPoints;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Lesson lesson = (Lesson) o;
+        return Objects.equals(id, lesson.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", recFinPoints=" + recFinPoints +
+                ", course=" + course +
+                '}';
+    }
+}
